a24a49fc75bcbc365dc87deb281c9664e96f7733
Christian Brannon <cabrannon54955@mymail.tstc.edu>
2018-02-26 14:36:33 -0600

    Add files via upload

---
+++ ArrayProgram.java
@@ -0,0 +1,59 @@
+
+//Christian Brannon, ID: 1593881
+//ITSE 2317-5001, 01.22.18
+//Assignment: Pay Calculator
+
+//ArrayProgram: Main File
+
+/*
+ *This program stores 10 integers into an array, and then 
+ *displays the array elements, then in reverse order,
+ *then a sum total, then all values less than a limiting 
+ *argument, and finally all values higher than the average.
+ */
+ 
+package ArrayProgram
+
+import java.util.Scanner;
+
+public class ArrayProgram
+{
+	int[] intArray;
+	
+	public static void main(String[] args)
+	{
+		
+		for (int i = 0; i < 10; i++)
+		{
+			
+		}
+	}
+	
+	public static void displayAll
+	{
+		for (intArray.length)
+		{
+			
+		}
+	}
+	
+	public static void displayAllReverse
+	{
+		
+	}
+	
+	public static void displaySum
+	{
+		
+	}
+	
+	public static void displayLessThan
+	{
+		
+	}
+	
+	public static void displayMoreThanAverage
+	{
+		
+	}
+}
